d06e409dc907e8efd6881e8eb69d09856122f8ae
diegorrod <30572826+diegorrod@users.noreply.github.com>
2019-04-27 21:39:25 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export const Greeter = (name: string) => `Hello ${name}`; 
+import firebase = require( "firebase/app" );
+import _Vue from 'vue';
+import Modules from './modules';
+
+export class DeskSuite {
+  public Modules: Modules;
+  
+  constructor(app: firebase.app.App)
+  {
+    this.Modules = new Modules(app);
+  }
+}
+
+export default function (Vue: typeof _Vue, options?: any): void {
+ 
+    const firebaseApp = firebase.initializeApp(options.config);
+    const deskSuite = new DeskSuite(firebaseApp);
+    Vue.prototype.$DeskSuite = deskSuite;
+}
